b6d8cd8e4c28c31e4a81ccf75ae52591ad5ff62f
Isaac <16ismith98@gmail.com>
2021-05-09 18:27:46 -0400

    Use onSuccess param for firebase functions

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -93,15 +93,19 @@ export function updateItem(
   itemInfo,
   oldId,
   setErrorAlert,
-  setSuccessAlert
+  setSuccessAlert,
+  onSuccess = () => {}
 ) {
   const itemRef = firebase.database().ref(`${path}/${itemInfo.id}`);
   itemRef
-    .update(itemInfo)
-    .then(() => {
-      if (itemInfo.id !== oldId) removeFromDatabase(path, oldId, setErrorAlert);
+    .update(itemInfo, () => {
+      let afterRemove = () => {
+        setSuccessAlert("Update Successful");
+        onSuccess();
+      };
+      if (itemInfo.id !== oldId)
+        removeFromDatabase(path, oldId, setErrorAlert, () => {}, afterRemove);
     })
-    .then(() => setSuccessAlert("Update Successful"))
     .catch((error) => setErrorAlert("Update failed: " + error));
 }
 
@@ -109,65 +113,76 @@ export function removeFromDatabase(
   path,
   id,
   setErrorAlert,
-  setSuccessAlert = console.log
+  setSuccessAlert,
+  onSuccess = () => {}
 ) {
   const ref = firebase.database().ref(`${path}/${id}`);
   ref
-    .remove()
-    .then(() => setSuccessAlert("Remove Successful"))
+    .remove(() => {
+      setSuccessAlert("Remove Successful");
+      onSuccess();
+    })
     .catch((error) => setErrorAlert("Remove Failed: " + error.message));
 }
 
-export async function orderDelivered(order, setErrorAlert, setSuccessAlert) {
-  try {
-    await checkInventoryForItems(order.itemsInCart);
-    addToCompletedOrders(order, setErrorAlert);
-    setSuccessAlert(`Order Delivered`);
-  } catch (e) {
-    setErrorAlert(e.message);
-  }
-}
-
-async function checkInventoryForItems(itemsInCart) {
+export function orderDelivered(order, setErrorAlert, setSuccessAlert) {
   const foodItemsRef = firebase.database().ref(`foodItems/`);
 
-  return foodItemsRef.transaction((foodItems) => {
-    if (foodItems) {
-      let updatedItems = itemsInCart.map((oneItemFromCart) => {
-        let foodItem = foodItems[oneItemFromCart.id];
+  return foodItemsRef.transaction(
+    (foodItems) => {
+      try {
+        if (foodItems) {
+          let updatedItems = order.itemsInCart.map((oneItemFromCart) => {
+            let foodItem = foodItems[oneItemFromCart.id];
 
-        if (foodItem) {
-          let itemsAvailable =
-            foodItem.totalInventory - oneItemFromCart.amount >= 0; //Change to inventory available eventaully
+            if (foodItem) {
+              let itemsAvailable =
+                foodItem.totalInventory - oneItemFromCart.amount >= 0; //Change to inventory available eventaully
 
-          if (itemsAvailable) {
-            foodItem.totalInventory -= oneItemFromCart.amount;
-            foodItem.amountReserved -= oneItemFromCart.amount;
-            let id = oneItemFromCart.id;
-            return { [id]: foodItem };
-          }
-          throw new Error(`Not enough ${foodItem.name}(s) available`);
-        } else {
-          throw new Error(
-            `${oneItemFromCart.name} deleted. Visit *link w/ instructions* to resolve this issue`
-          );
+              if (itemsAvailable) {
+                foodItem.totalInventory -= oneItemFromCart.amount;
+                foodItem.amountReserved -= oneItemFromCart.amount;
+                let id = oneItemFromCart.id;
+                return { [id]: foodItem };
+              }
+              throw new Error(`Not enough ${foodItem.name}(s) available`);
+            } else {
+              throw new Error(
+                `${oneItemFromCart.name} deleted. Visit *link w/ instructions* to resolve this issue`
+              );
+            }
+          });
+
+          return Object.assign(foodItems, ...updatedItems);
         }
-      });
-
-      return Object.assign(foodItems, ...updatedItems);
+      } catch (error) {
+        setErrorAlert(error.message);
+      }
+    },
+    (error, committed) => {
+      if (error) setErrorAlert(error);
+      else if (committed) {
+        let afterOrderAdded = () => setSuccessAlert(`Order Delivered`);
+        addToCompletedOrders(order, setErrorAlert, afterOrderAdded);
+      }
     }
-  });
+  );
 }
 
-export async function addToCompletedOrders(order, setErrorAlert) {
+export function addToCompletedOrders(order, setErrorAlert, onSuccess) {
   const completedOrdersRef = firebase
     .database()
     .ref(`completedOrders/${order.orderId}`);
 
-  try {
-    await completedOrdersRef.update(order);
-    removeFromDatabase("orders", order.orderId, setErrorAlert);
-  } catch (error) {
-    setErrorAlert("Failed to add this order to list of completed orders");
-  }
+  completedOrdersRef.update(order, (error) => {
+    if (error)
+      setErrorAlert("Failed to add this order to list of completed orders");
+    removeFromDatabase(
+      "orders",
+      order.orderId,
+      setErrorAlert,
+      () => {},
+      onSuccess
+    );
+  });
 }
